9ab94dabb5316ba19bfdb4cee5d319fcc34f2942
noppharat0th <uiavgcgccggv@gmail.com>
2026-05-14 00:40:21 +0700

    update security-detect-debugger

--- protect-decomplie/security.hpp
+++ protect-decomplie/security.hpp
@@ -6,4 +6,23 @@ template <size_t N, char KEY>
 
 class security {
 
+private:
+	char buffer[N];
+	constexpr char encrypt(char c) const { return c ^ KEY; }
+
+public:
+	template <size_t... I>
+	constexpr ObfuscatedString(const char(&str)[N], std::index_sequence<I...>)
+		: buffer{ encrypt(str[I])... } {
+	}
+
+	__forceinline bool detectDebugger()
+	{	
+		#ifdef _WIN64
+			unsigned char* peb = (unsigned char*)__readgsqword(0x60);
+			return peb[2] != 0;
+		#else
+			return false;
+		#endif
+	}
 };
